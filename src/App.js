cfe89191be9b9b85a1dabcba1be96b525e1551e7
Owais Mansha <awaismansha1998@gmail.com>
2020-09-25 15:27:23 +0500

    use effect and async

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [mydata, setMydata] = useState()
+  const [isData, setData] = useState(false)
+  const [Fetching, setFetching] = useState(false)
+  useEffect(() => {
+
+    async function FetchData() {
+      setFetching(true)
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/101')
+      console.log("Response = ", response)
+      let res = await response.json();
+      setMydata(res)
+      setFetching(false)
+    }
+
+    FetchData()
+  }, [isData])
+
+  if (Fetching) {
+    return (
+      <div className="App">
+        <h1>Loading Data ...</h1></div>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Work</h1>
+      <h1>Title :{mydata == null ? "Fetching Data" : mydata.title}</h1>
+      <h3>{JSON.stringify(mydata)}</h3>
     </div>
   );
 }
